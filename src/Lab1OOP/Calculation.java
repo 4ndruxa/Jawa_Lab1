fcede6e7489a3c0a0818d5879077aae8a6de71a8
Andrew <125910808+4ndruxa@users.noreply.github.com>
2023-02-20 20:56:10 +0200

    Лабораторна робота №1 ООП (з коментарями)
    123 ОТ ФІОТ
    КПІ
    1 курс
    Боднар Андрій
    
    "Основні типи та оператори мови програмування Java"

--- src/Lab1OOP/Calculation.java
+++ src/Lab1OOP/Calculation.java
@@ -3,9 +3,9 @@ package Lab1OOP;
 import java.util.Scanner;
 public class Calculation {
     public static void calculator (){
-
+        //Саме обчислення виразу
         System.out.println("\nПриступимо до обчислення виразу:");
-
+        //Можливість ввести будь які дані
         Scanner scan1 = new Scanner(System.in);
         System.out.print("\nВведіть значення а: ");
         byte a = scan1.nextByte();
@@ -15,20 +15,31 @@ public class Calculation {
         byte n = scan1.nextByte();
         System.out.print("Введіть значення m: ");
         byte m = scan1.nextByte();
+        //Константа С
         byte c = 2;
 
         double result = 0;
-
+        //Алгоритм обчислення
         for (byte i = a; i <= n; i++) {
             for (byte j = b; j <= m; j++) {
+                //Обходження варіантів у яких порушуюється ОДЗ
                 if (i == 2){
                     System.out.println("Пропщено крок з діленням на 0;");
                 }
                 else {
+                    //Вираз, який обчислюється
                     result += (i + j) / (i - c);
                 }
             }
         }
+        //Відповідь :з
         System.out.println("\nРезультат обчислення: " + result);
     }
 }
+/* P.S. Якщо змінити в 19 рядку значення змінної С, а в 31 - операції (в першій та другій дужці), то можна
+експерементувати з варіантами, але не варто забувати про область допустимих значень у 26 рядку і типи данних (11-24 р.),
+які треба теж в залежності від виразу змінити.
+Дякую за увагу)
+Слава Україні!
+ */
+
--- src/Lab1OOP/Main.java
+++ src/Lab1OOP/Main.java
@@ -1,12 +1,12 @@
 package Lab1OOP;
 public class Main {
     public static void main(String[] args) {
-
+        //Представляюсь
         System.out.println("Лабораторна робота №1\nВиконав: студент групи ІО - 23\nБоднар Андрій");
-
+        //Виклик методу Variant, котрий обчислює варіант
         Variant v = new Variant();
         v.variant();
-
+        //Виклик методу Calculation
         Calculation calk = new Calculation();
         calk.calculator();
     }
